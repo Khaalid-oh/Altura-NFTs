12f9a222def001a7fa9ed801eaed34cc44d51ece
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-05-11 21:09:30 +0100

    logo update

--- src/components/organisms/Navbar/index.tsx
+++ src/components/organisms/Navbar/index.tsx
@@ -1,9 +1,11 @@
+import NFTX from '../../../assets/NFTX.png'
+
+
 const Navbar = () => {
   return (
-    <nav className="flex px-6 py-4 border border-b-[#EFEFEF]">
+    <nav className="flex px-6 border border-b-[#EFEFEF]">
       <div className="flex items-center">
-        <img src="https://www.alturanft.com/logo-svg.svg" alt="Altura-NFT" />
-        <span className="text-xl font-medium">AlturaNFT</span>
+        <img src={NFTX} alt="NFTX" className='h-14'/>
       </div>
     </nav>
   );
