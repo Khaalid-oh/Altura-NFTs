23ffebbc6a59200027518f77e439f94a5655d244
Khalid Sulyman <84248666+Khaliid-oh@users.noreply.github.com>
2023-11-14 20:31:00 +0100

    Text copy update

--- src/How.tsx
+++ src/How.tsx
@@ -5,51 +5,49 @@ import React from "react";
 function How() {
 
   return (
-        <div className="h-full w-full md:w-3/4 flex flex-col gap-6 place-self-center">
-          <div className="p-2 md:px-16 flex flex-col gap-2 md:gap-6 items-center">
-            <h1 className="text-3xl">NFT Explorer App</h1>
-            <p className="text-gray-600 md:w-4/5 flex text-center">
-              Explore and discover Non-Fungible Tokens (NFTs) like never before!
-              Our platform offers a unique feature that allows you to easily
-              view and track NFTs of any given address on the blockchain.
+    <div className="h-full w-full md:w-3/4 flex flex-col gap-6 place-self-center">
+      <div className="p-2 md:px-16 flex flex-col gap-2 md:gap-6 items-center">
+        <h1 className="text-3xl">NFT Explorer App</h1>
+        <p className="text-gray-600 md:w-4/5 flex text-center">
+          Explore and discover Non-Fungible Tokens (NFTs) like never before! Our
+          platform offers a unique feature that allows you to easily view and
+          track NFTs of any given address on the blockchain.
+        </p>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-2 ">
+        <h2 className="text-xl text-[#3D00B7] underline">How it works</h2>
+        <div className="flex flex-col gap-4">
+          <div className="bg-violet-200 rounded-xl p-2 md:w-1/2">
+            <p className="text-[#3D00B7]">01.</p>
+            <p className="text-gray-600">
+              Paste your Ethereum contract address to display all the list NFT
+              collection associated the wallet.
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center gap-2 ">
-            <h2 className="text-xl text-[#3D00B7] underline">How it works</h2>
-            <div className="flex flex-col gap-4">
-              <div className="bg-violet-200 rounded-xl p-2 md:w-1/2">
-                <p className="text-[#3D00B7]">01.</p>
-                <p className="text-gray-600">
-                  Paste your Ethereum contract address to display all the list
-                  NFT collection associated the wallet.
-                </p>
-              </div>
-              <div className="bg-violet-200 rounded-xl p-2 md:w-1/2 self-end ">
-                <p className="text-[#3D00B7]">02.</p>
-                <p className="text-gray-600">
-                  The app utilize the Alchemy API to retrieve the list of NFTs
-                  associated with the provided address.
-                </p>
-              </div>
-              <div className="bg-violet-200 rounded-xl p-2 md:w-1/2">
-                <p className="text-[#3D00B7]">03.</p>
-                <p className="text-gray-600">
-                  The app display the retrieved NFTs and digital art from the
-                  address to list on the app for you to explore.
-                </p>
-              </div>
-              <div className="bg-violet-200 rounded-xl p-2 md:w-1/2 self-end">
-                <p className="text-[#3D00B7]">04.</p>
-                <p className="text-gray-600">
-                  Copy and paste the following Azuki wallet address to test:
-                  <span className="text-xs">
-                    0xED5AF388653567Af2F388E6224dC7C4b3241C544
-                  </span>
-                </p>
-              </div>
-            </div>
+          <div className="bg-violet-200 rounded-xl p-2 md:w-1/2 self-end ">
+            <p className="text-[#3D00B7]">02.</p>
+            <p className="text-gray-600">
+              The app utilize the Alchemy API to retrieve the list of NFTs
+              associated with the provided address.
+            </p>
+          </div>
+          <div className="bg-violet-200 rounded-xl p-2 md:w-1/2">
+            <p className="text-[#3D00B7]">03.</p>
+            <p className="text-gray-600">
+              The app display the retrieved NFTs and digital art from the
+              address to list on the app for you to explore.
+            </p>
+          </div>
+          <div className="bg-violet-200 rounded-xl p-2 md:w-1/2 self-end">
+            <p className="text-[#3D00B7]">04.</p>
+            <p className="text-gray-600 select-text">
+              Copy and paste the following Azuki wallet address to test:
+              0xED5AF388653567Af2F388E6224dC7C4b3241C544
+            </p>
           </div>
         </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/organisms/Navbar/index.tsx
+++ src/components/organisms/Navbar/index.tsx
@@ -1,10 +1,10 @@
-import NFTX from '../../../assets/NFTX.png'
+
 
 
 const Navbar = () => {
   return (
     <nav className="flex px-6 py-2 border border-b-[#EFEFEF]">
-      <div className="flex items-center text-purple-600 text-3xl font-semibold">
+      <div className="flex items-center text-purple-700 text-3xl font-semibold">
         NFTx
       </div>
     </nav>
